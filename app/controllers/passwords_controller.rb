450fedd01c5beee18e1485bc254a35df6cb80085
Eric Todasco <erictodasco@me.com>
2025-01-16 13:42:41 -0500

    updated user password edit

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -5,5 +5,16 @@ before_action :require_user_logged_in!
     end
 
     def update
+        if Current.user.update(password_params)
+            redirect_to root_path, notice: "Password Updated!"
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
     end
 end
