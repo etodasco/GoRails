98cb804cf0a49d8d4b06c4d2c85bc073ec67316b
Eric Todasco <erictodasco@me.com>
2025-01-16 14:02:11 -0500

    updated password reset actions

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,10 @@ post "sign_up", to: "registrations#create"
 get "sign_in", to: "sessions#new"
 post "sign_in", to: "sessions#create"
 
-
 delete "logout", to: "sessions#destroy"
+
+get "password/reset", to: "password_resets#new"
+post "password/reset", to: "password_resets#create"
 
 root to: "main#index"
 end
