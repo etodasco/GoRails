0698fc47c0ecb17de20e1fbb8bfd9eaca4121335
Eric Todasco <erictodasco@me.com>
2025-01-17 14:14:49 -0500

    created omniauth controller

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ post "password/reset", to: "password_resets#create"
 get "password/reset/edit", to: "password_resets#edit"
 patch "password/reset/edit", to: "password_resets#update"
 
+get "auth/twitter/callback", to: "omniauth_callbacks#twitter"
+
 root to: "main#index"
 end
